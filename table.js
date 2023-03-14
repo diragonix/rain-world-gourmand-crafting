31809f9b2611ca625e201249c1f3bf5fab6e93f9
CarlKj <carlkjellmans@gmail.com>
2023-03-14 12:41:56 +0200

    initial commit

---
+++ table.js
@@ -0,0 +1,334 @@
+items = [
+    {name:"Rubbish",name_id:"Rubbish",id: 0,recepies: []},
+    {name:"Flashbang",name_id:"Flashbang",id: 1,recepies: [{"x": 4,"y": 0},{"x": 9,"y": 0},{"x": 9,"y": 4},{"x": 9,"y": 7},{"x": 30,"y": 16},{"x": 32,"y": 15}]},
+    {name:"Vulture Mask",name_id:"Vulture_Mask",id: 2,recepies: [{"x": 30,"y": 10}]},
+    {name:"Spore Puff",name_id:"Spore_Puff",id: 3,recepies: [{"x": 11,"y": 0},{"x": 11,"y": 1},{"x": 11,"y": 9},{"x": 16,"y": 0},{"x": 16,"y": 14},{"x": 16,"y": 15},{"x": 17,"y": 16},{"x": 19,"y": 15},{"x": 19,"y": 16},{"x": 24,"y": 11},{"x": 24,"y": 15},{"x": 24,"y": 16},{"x": 24,"y": 19},{"x": 27,"y": 0},{"x": 27,"y": 15},{"x": 29,"y": 10},{"x": 31,"y": 0},{"x": 31,"y": 1},{"x": 31,"y": 11},{"x": 31,"y": 15},{"x": 31,"y": 16}]},
+    {name:"Blue Fruit",name_id:"Blue_Fruit",id: 4,recepies: []},
+    {name:"Neuron Fly",name_id:"Neuron_Fly",id: 5,recepies: [{"x": 20,"y": 10}]},
+    {name:"Pearl",name_id:"Pearl",id: 6,recepies: [{"x": 2,"y": 0},{"x": 2,"y": 1},{"x": 3,"y": 2},{"x": 4,"y": 2},{"x": 7,"y": 2},{"x": 8,"y": 2},{"x": 9,"y": 2},{"x": 10,"y": 2},{"x": 10,"y": 9},{"x": 11,"y": 2},{"x": 12,"y": 2},{"x": 13,"y": 2},{"x": 14,"y": 2},{"x": 15,"y": 0},{"x": 15,"y": 2},{"x": 15,"y": 9},{"x": 16,"y": 2},{"x": 17,"y": 2},{"x": 19,"y": 2},{"x": 20,"y": 0},{"x": 20,"y": 1},{"x": 20,"y": 2},{"x": 20,"y": 3},{"x": 20,"y": 4},{"x": 20,"y": 5},{"x": 20,"y": 7},{"x": 20,"y": 8},{"x": 20,"y": 9},{"x": 20,"y": 11},{"x": 20,"y": 12},{"x": 20,"y": 13},{"x": 20,"y": 14},{"x": 20,"y": 15},{"x": 20,"y": 16},{"x": 20,"y": 17},{"x": 20,"y": 18},{"x": 20,"y": 19},{"x": 21,"y": 20},{"x": 22,"y": 20},{"x": 23,"y": 20},{"x": 24,"y": 2},{"x": 24,"y": 20},{"x": 25,"y": 2},{"x": 25,"y": 20},{"x": 26,"y": 2},{"x": 26,"y": 20},{"x": 27,"y": 2},{"x": 27,"y": 20},{"x": 28,"y": 20},{"x": 29,"y": 20},{"x": 30,"y": 9},{"x": 30,"y": 20},{"x": 31,"y": 20},{"x": 32,"y": 20}]},
+    {name:"Bubble Fruit",name_id:"Bubble_Fruit",id: 7,recepies: []},
+    {name:"Jellyfish",name_id:"Jellyfish",id: 8,recepies: [{"x": 7,"y": 0},{"x": 7,"y": 1},{"x": 25,"y": 16},{"x": 25,"y": 19},{"x": 26,"y": 16},{"x": 29,"y": 19},{"x": 32,"y": 12},{"x": 32,"y": 16}]},
+    {name:"Lantern",name_id:"Lantern",id: 9,recepies: [{"x": 1,"y": 0},{"x": 10,"y": 1},{"x": 12,"y": 1},{"x": 13,"y": 0},{"x": 13,"y": 1},{"x": 13,"y": 12},{"x": 15,"y": 1},{"x": 15,"y": 12},{"x": 15,"y": 13},{"x": 24,"y": 1},{"x": 25,"y": 0},{"x": 25,"y": 1},{"x": 26,"y": 0},{"x": 26,"y": 1},{"x": 26,"y": 15},{"x": 30,"y": 0},{"x": 30,"y": 1}]},
+    {name:"Karma Flower",name_id:"Karma_Flower",id: 10,recepies: [{"x": 22,"y": 21}]},
+    {name:"Mushroom",name_id:"Mushroom",id: 11,recepies: [{"x": 3,"y": 1},{"x": 4,"y": 3},{"x": 9,"y": 3},{"x": 13,"y": 3},{"x": 13,"y": 9},{"x": 14,"y": 3},{"x": 16,"y": 4},{"x": 19,"y": 3},{"x": 19,"y": 13},{"x": 19,"y": 14},{"x": 19,"y": 17},{"x": 24,"y": 3},{"x": 24,"y": 14},{"x": 25,"y": 3},{"x": 27,"y": 16},{"x": 28,"y": 0},{"x": 28,"y": 1},{"x": 28,"y": 3},{"x": 28,"y": 9},{"x": 28,"y": 14},{"x": 29,"y": 14},{"x": 30,"y": 14},{"x": 31,"y": 3},{"x": 31,"y": 14},{"x": 31,"y": 19}]},
+    {name:"Cherrybomb",name_id:"Cherrybomb",id: 12,recepies: [{"x": 14,"y": 0},{"x": 14,"y": 1},{"x": 14,"y": 9},{"x": 14,"y": 13},{"x": 15,"y": 4},{"x": 15,"y": 14},{"x": 16,"y": 1},{"x": 16,"y": 9},{"x": 16,"y": 13},{"x": 17,"y": 15},{"x": 19,"y": 0},{"x": 19,"y": 1},{"x": 28,"y": 15},{"x": 29,"y": 9},{"x": 29,"y": 15},{"x": 30,"y": 3},{"x": 30,"y": 15}]},
+    {name:"Slime Mold",name_id:"Slime_Mold",id: 13,recepies: [{"x": 4,"y": 1}]},
+    {name:"Batnip",name_id:"Batnip",id: 14,recepies: [{"x": 12,"y": 8},{"x": 28,"y": 10},{"x": 28,"y": 16},{"x": 28,"y": 19}]},
+    {name:"Grenade",name_id:"Grenade",id: 15,recepies: [{"x": 10,"y": 0},{"x": 12,"y": 0},{"x": 12,"y": 3},{"x": 12,"y": 9},{"x": 12,"y": 10},{"x": 16,"y": 10},{"x": 16,"y": 12},{"x": 22,"y": 0},{"x": 22,"y": 1},{"x": 22,"y": 2},{"x": 22,"y": 3},{"x": 22,"y": 9},{"x": 22,"y": 11},{"x": 22,"y": 12},{"x": 22,"y": 14},{"x": 22,"y": 16},{"x": 22,"y": 19}]},
+    {name:"Beehive",name_id:"Beehive",id: 16,recepies: [{"x": 3,"y": 0},{"x": 12,"y": 4},{"x": 14,"y": 12},{"x": 15,"y": 11},{"x": 17,"y": 1},{"x": 17,"y": 12},{"x": 24,"y": 0},{"x": 24,"y": 12},{"x": 25,"y": 12},{"x": 25,"y": 15},{"x": 26,"y": 12},{"x": 27,"y": 1},{"x": 27,"y": 12},{"x": 28,"y": 12},{"x": 29,"y": 0},{"x": 29,"y": 1},{"x": 29,"y": 3},{"x": 29,"y": 12},{"x": 30,"y": 12},{"x": 31,"y": 12}]},
+    {name:"Eggbug",name_id:"Eggbug",id: 17,recepies: []},
+    {name:"Noodlefly Egg",name_id:"Noodlefly_Egg",id: 18,recepies: [{"x": 10,"y": 3},{"x": 10,"y": 8},{"x": 11,"y": 7},{"x": 11,"y": 10},{"x": 17,"y": 0},{"x": 17,"y": 9},{"x": 17,"y": 10},{"x": 31,"y": 9},{"x": 31,"y": 10}]},
+    {name:"Bubbleweed",name_id:"Bubble_Weed",id: 19,recepies: [{"x": 12,"y": 7},{"x": 14,"y": 7},{"x": 14,"y": 8},{"x": 32,"y": 3},{"x": 32,"y": 10},{"x": 32,"y": 11},{"x": 32,"y": 14}]},
+    {name:"Overseer",name_id:"Overseer",id: 20,recepies: [{"x": 5,"y": 0},{"x": 5,"y": 1},{"x": 5,"y": 2},{"x": 5,"y": 3},{"x": 6,"y": 5},{"x": 9,"y": 5},{"x": 10,"y": 5},{"x": 11,"y": 5},{"x": 12,"y": 5},{"x": 14,"y": 5},{"x": 15,"y": 5},{"x": 16,"y": 5},{"x": 18,"y": 5},{"x": 18,"y": 10},{"x": 19,"y": 5},{"x": 21,"y": 5},{"x": 23,"y": 10}]},
+    {name:"Singularity Bomb",name_id:"Singularity_Bomb",id: 21,recepies: [{"x": 22,"y": 10},{"x": 22,"y": 15}]},
+    {name:"Fire Egg",name_id:"Fire_Egg",id: 22,recepies: [{"x": 15,"y": 10},{"x": 21,"y": 0},{"x": 21,"y": 1},{"x": 21,"y": 2},{"x": 21,"y": 3},{"x": 21,"y": 4},{"x": 21,"y": 7},{"x": 21,"y": 8},{"x": 21,"y": 9},{"x": 21,"y": 10},{"x": 21,"y": 12},{"x": 21,"y": 13},{"x": 21,"y": 14},{"x": 21,"y": 15},{"x": 21,"y": 16},{"x": 21,"y": 17},{"x": 21,"y": 19},{"x": 23,"y": 21},{"x": 24,"y": 21},{"x": 25,"y": 21},{"x": 26,"y": 21},{"x": 27,"y": 21},{"x": 28,"y": 21},{"x": 29,"y": 21},{"x": 30,"y": 21},{"x": 31,"y": 21},{"x": 32,"y": 21}]},
+    {name:"Seed",name_id:"Seed",id: 23,recepies: [{"x": 10,"y": 4},{"x": 10,"y": 7},{"x": 13,"y": 10},{"x": 14,"y": 10},{"x": 19,"y": 10},{"x": 24,"y": 10},{"x": 25,"y": 10},{"x": 26,"y": 10},{"x": 27,"y": 10},{"x": 28,"y": 2},{"x": 28,"y": 18},{"x": 29,"y": 2},{"x": 29,"y": 18},{"x": 30,"y": 2},{"x": 30,"y": 18},{"x": 31,"y": 2},{"x": 31,"y": 18},{"x": 32,"y": 2},{"x": 32,"y": 18}]},
+    {name:"Gooieduck",name_id:"Gooieduck",id: 24,recepies: [{"x": 7,"y": 3},{"x": 8,"y": 3},{"x": 11,"y": 3},{"x": 11,"y": 4},{"x": 11,"y": 8},{"x": 14,"y": 4},{"x": 15,"y": 3},{"x": 16,"y": 3},{"x": 16,"y": 11},{"x": 17,"y": 3},{"x": 17,"y": 11},{"x": 17,"y": 14},{"x": 25,"y": 11},{"x": 26,"y": 3},{"x": 26,"y": 11},{"x": 27,"y": 3},{"x": 27,"y": 11},{"x": 28,"y": 11},{"x": 29,"y": 11},{"x": 30,"y": 11}]},
+    {name:"Lillypuck",name_id:"Lillypuck",id: 25,recepies: [{"x": 8,"y": 0},{"x": 8,"y": 1},{"x": 19,"y": 4},{"x": 19,"y": 7},{"x": 19,"y": 8},{"x": 24,"y": 9},{"x": 26,"y": 9},{"x": 26,"y": 19},{"x": 32,"y": 0}]},
+    {name:"Glow Weed",name_id:"Glow_Weed",id: 26,recepies: [{"x": 9,"y": 1},{"x": 9,"y": 8},{"x": 19,"y": 9},{"x": 25,"y": 9},{"x": 27,"y": 9},{"x": 27,"y": 19},{"x": 30,"y": 19},{"x": 32,"y": 1},{"x": 32,"y": 19}]},
+    {name:"Dandelion Peach",name_id:"Dandelion_Peach",id: 27,recepies: []},
+    {name:"Batfly",name_id:"Batfly",id: 28,recepies: [{"x": 14,"y": 11},{"x": 18,"y": 0},{"x": 18,"y": 4},{"x": 18,"y": 14},{"x": 23,"y": 0},{"x": 23,"y": 14},{"x": 27,"y": 14}]},
+    {name:"Centipede",name_id:"Centipede",id: 29,recepies: [{"x": 12,"y": 11},{"x": 18,"y": 3},{"x": 18,"y": 11},{"x": 18,"y": 12},{"x": 18,"y": 13},{"x": 18,"y": 15},{"x": 21,"y": 11},{"x": 23,"y": 3},{"x": 23,"y": 11},{"x": 23,"y": 12},{"x": 23,"y": 15},{"x": 26,"y": 14}]},
+    {name:"Vulture Grub",name_id:"Vulture_Grub",id: 30,recepies: [{"x": 18,"y": 1},{"x": 18,"y": 2},{"x": 18,"y": 9},{"x": 21,"y": 18},{"x": 22,"y": 18},{"x": 23,"y": 1},{"x": 23,"y": 2},{"x": 23,"y": 9}]},
+    {name:"Noodlefly",name_id:"Noodlefly",id: 31,recepies: [{"x": 13,"y": 11},{"x": 18,"y": 17},{"x": 23,"y": 18},{"x": 27,"y": 18}]},
+    {name:"Hazer",name_id:"Hazer",id: 32,recepies: [{"x": 16,"y": 7},{"x": 16,"y": 8},{"x": 18,"y": 7},{"x": 19,"y": 11},{"x": 19,"y": 18},{"x": 23,"y": 19},{"x": 25,"y": 14},{"x": 25,"y": 18},{"x": 26,"y": 18}]},
+    {name:"Snail",name_id:"Snail",id: 33,recepies: [{"x": 15,"y": 7},{"x": 15,"y": 8},{"x": 18,"y": 8},{"x": 19,"y": 12},{"x": 32,"y": 9}]},
+    {name:"Grappling Worm",name_id:"Grappling_Worm",id: 34,recepies: [{"x": 18,"y": 16},{"x": 23,"y": 16},{"x": 24,"y": 18},{"x": 29,"y": 16}]}
+    ]
+
+craftingList = [
+    [-1],
+    [9,-1],
+    [6,6,-1],
+    [16,11,6,-1],
+    [1,13,6,11,-1],
+    [20,20,20,20,{f:3},-1],
+    [-1,-1,-1,-1,-1,20,-1],
+    [8,8,6,24,{f:3},{f:3},-1,-1],
+    [25,25,6,24,{f:3},{f:3},-1,{f:3},-1],
+    [1,26,6,11,1,20,-1,1,26,-1],
+    [15,9,6,18,23,20,-1,23,18,6,-1],
+    [3,3,6,24,24,20,-1,18,24,3,18,-1],
+    [15,9,6,15,16,20,-1,19,14,15,15,29,-1],
+    [9,9,6,11,{f:3},{f:3},-1,{f:3},{f:3},11,23,31,9,-1],
+    [12,12,6,11,24,20,-1,19,19,12,23,28,16,12,-1],
+    [6,9,6,24,12,20,-1,33,33,6,22,16,9,9,12,-1],
+    [3,12,6,24,11,20,-1,32,32,12,15,24,15,12,3,3,-1],
+    [18,16,6,24,{f:3},{f:3},-1,{f:3},{f:3},18,18,24,16,{f:3},24,12,3,-1],
+    [28,30,30,29,28,20,-1,32,33,30,20,29,29,29,28,29,34,31,-1],
+    [12,12,6,11,25,20,-1,25,25,26,23,32,33,11,11,3,3,11,32,-1],
+    [6,6,6,6,6,6,-1,6,6,6,5,6,6,6,6,6,6,6,6,6,-1],
+    [22,22,22,22,22,20,-1,22,22,22,22,29,22,22,22,22,22,22,30,22,6,-1],
+    [15,15,15,15,{f:3},{f:3},-1,{f:3},{f:3},15,21,15,15,{f:3},15,21,15,{f:3},30,15,6,10,-1],
+    [28,30,30,29,{f:3},{f:3},-1,{f:3},{f:3},30,20,29,29,{f:3},28,29,34,{f:3},31,32,6,22,{f:3},-1],
+    [16,9,6,11,{f:4},{f:4},-1,{f:4},{f:4},25,23,3,16,{f:4},11,3,3,{f:4},34,3,6,22,{f:4},{f:4},-1],
+    [9,9,6,11,{f:3},{f:3},-1,{f:3},{f:3},26,23,24,16,{f:3},32,16,8,{f:3},32,8,6,22,{f:3},{f:3},{f:4},-1],
+    [9,9,6,24,{f:3},{f:3},-1,{f:3},{f:3},25,23,24,16,{f:3},29,9,8,{f:3},32,25,6,22,{f:3},{f:3},{f:4},{f:3},-1],
+    [3,16,6,24,{f:3},{f:3},-1,{f:3},{f:3},26,23,24,16,{f:3},28,3,11,{f:3},31,26,6,22,{f:3},{f:3},{f:4},{f:3},{f:3},-1],
+    [11,11,23,11,{f:3},{f:3},-1,{f:3},{f:3},11,14,24,16,{f:3},11,12,14,{f:3},23,14,6,22,{f:3},{f:3},{f:4},{f:3},{f:3},{f:3},-1],
+    [16,16,23,16,{f:4},{f:4},-1,{f:4},{f:4},12,3,24,16,{f:4},11,12,34,{f:4},23,8,6,22,{f:4},{f:4},{f:5},{f:4},{f:4},{f:4},{f:4},-1],
+    [9,9,23,12,{f:3},{f:3},-1,{f:3},{f:3},6,2,24,16,{f:3},11,12,1,{f:3},23,26,6,22,{f:3},{f:3},{f:4},{f:3},{f:3},{f:3},{f:3},{f:4},-1],
+    [3,3,23,11,{f:4},{f:4},-1,{f:4},{f:4},18,18,3,16,{f:4},11,3,3,{f:4},23,11,6,22,{f:4},{f:4},{f:5},{f:4},{f:4},{f:4},{f:4},{f:5},{f:4},-1],
+    [25,26,23,19,{f:3},{f:3},-1,{f:3},{f:3},33,19,19,8,{f:3},19,1,8,{f:3},23,26,6,22,{f:3},{f:3},{f:4},{f:3},{f:3},{f:3},{f:3},{f:4},{f:3},{f:4},-1]
+]
+
+const craftItemButton = document.getElementById("craft-items");
+
+craftingMode = 0; // 0 == find recepies from what items you have, 1 ==  find items you need to craft something
+
+craftingArray = [];
+
+selectedObject = {array:null,id:null};
+
+function getCraftingRecepies(id,array) { // select one item and get all recepies for it
+    let craftList = []
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            if (array[i][j] == id) {
+                craftList.push({x:i,y:j});
+};
+}};
+    return craftList;
+}
+
+function createCraftingChunk(currentItems = [{type:0}],array) {
+    
+}
+
+function craftItem(x,y) {
+	
+	if (x>=y) {
+    food1 = x;
+	food2 = y;
+  } else {
+    food1 = y;
+	food2 = x;
+  }
+    // console.log(food1,food2);
+    if (food1 >= 33 || x == -1 || y == -1) {
+        return -1;
+    }
+	// console.log(`${items[[food1]].name} + ${items[[food2]].name} = ${items[craftingList[food1][food2]]}`)
+    return craftingList[food1][food2];
+
+}
+
+function createItemButtons(id) {
+    for (let i = 0; i < items.length; i++) {
+        let item = document.createElement("img");
+        item.src = `./portraits/${items[i].name_id}_icon.png`;
+        item.title = items[i].name;
+        item.addEventListener('click',(e) => {
+            if (craftingMode == 0) {
+                // console.log(items[i].name);
+                setSelectedBoxItem(items[i].id);
+            }
+        });
+        item.classList.add("item-image","item-box");
+        document.getElementById(id).appendChild(item);
+    }
+    let plus = document.createElement("img");
+        plus.title = "Add item";
+        plus.classList.add("item-image","item-box");
+        plus.src = `./portraits/Plus_icon.png`;
+        plus.addEventListener('click',(e) => {
+            if (craftingMode == 0) {   
+                changeAndToComma();
+                haveContainer.appendChild(textElement("and",["box-text","and"]));
+                createItemBox(haveContainer,craftingArray,"have-box-");
+                selectBox(selectedObject.array,selectedObject.id + 1);   
+            }
+        })
+        document.getElementById(id).appendChild(plus);
+}
+
+createItemButtons("items");
+
+function createItemBox(parentElement,boxArray,id,defaultItem = -1,decorative = false) {
+    let box = document.createElement("div");
+    box.classList.add("item-image","item-box");
+    box.id = `${id}${boxArray.length}`
+    let boxID = boxArray.length;
+    let img = document.createElement("img");
+    img.classList.add("box-img");
+    if (!decorative) {
+        box.addEventListener('click',(e) => {
+            selectBox(boxArray,boxID);
+        });
+    }
+    if (defaultItem != -1) {   
+        if (defaultItem.f == undefined) {   
+            img.src = `./portraits/${items[defaultItem].name_id}_icon.png`;
+        } else {
+            img.src = `./portraits/Food_pip_icon.png`;
+        }
+    } else {
+        img.src = `./portraits/Remove_icon.png`;
+    }
+    box.appendChild(img);
+    if (!decorative) {
+    boxArray.push({id:box.id,currentItem:defaultItem});
+    }
+    parentElement.appendChild(box);
+}
+
+function create(params) {
+    
+}
+
+function selectBox(boxArray,id) {
+    if (selectedObject.array !== null && selectedObject.id !== null) {
+        // console.log(boxArray,id,"\n",selectedObject.array,selectedObject.id);
+        if (selectedObject.array !== boxArray || selectedObject.id !== id) {
+            document.getElementById(selectedObject.array[selectedObject.id].id).classList.remove("selected");
+            selectedObject.array = boxArray;
+            selectedObject.id = id;
+            document.getElementById(boxArray[id].id).classList.add("selected");
+        }
+    } else {
+        selectedObject.array = boxArray;
+        selectedObject.id = id;
+        document.getElementById(boxArray[id].id).classList.add("selected");
+    }
+}
+
+function createHaveLayout() {
+    haveContainer = document.createElement('div');
+    haveContainer.id = "have-container";   
+    haveContainer.classList.add("text-option");
+    haveContainer.appendChild(textElement("I have","box-text"));
+    createItemBox(haveContainer,craftingArray,"have-box-");
+    haveContainer.appendChild(textElement("and",["box-text","and"]));
+    createItemBox(haveContainer,craftingArray,"have-box-");
+    // createItemBox(haveContainer,craftingArray,"have-box-");
+    // createItemBox(haveContainer,craftingArray,"have-box-");
+    // createItemBox(haveContainer,craftingArray,"have-box-");
+    // console.log(craftingArray);
+
+    document.getElementById("text-selector").appendChild(haveContainer);
+    selectBox(craftingArray,0);
+    // I have <span id="have1" class="span-selection have selected item-box"></span> and <span id="have2" class="span-selection have item-box"></span></div>
+}
+
+createHaveLayout();
+
+function changeAndToComma() {
+    let and = document.getElementsByClassName("and");
+    
+    and[0].innerText = " , ";
+
+    and[0].classList.add("comma");
+    and[0].classList.remove("and");
+}
+
+function textElement(string,class_,id) {
+    let text = document.createElement("p");
+    if (class_ !== undefined) {
+        if (!Array.isArray(class_)) {
+            text.classList.add(class_);
+        } else {
+            for (let i = 0; i < class_.length; i++) {
+                text.classList.add(class_[i]);
+            }
+        }
+    }
+    if (id !== undefined) {
+        text.id = id;
+    }
+    text.innerText = string;
+    return text;
+}
+
+function setSelectedBoxItem(id) {
+    let currentBox = selectedObject.array[selectedObject.id];
+    let box = document.getElementById(currentBox.id);
+    let img = box.children[0];
+    let newSrc = `./portraits/${items[id].name_id}_icon.png`;
+    if (currentBox.currentItem !== id) {
+        img.src = newSrc
+        currentBox.currentItem = id;
+        //console.log(selectedObject.array.length,selectedObject.id);
+        if (selectedObject.array.length > selectedObject.id + 1) {
+         selectBox(selectedObject.array,selectedObject.id + 1);   
+        }
+    } 
+
+}
+const output = document.getElementById("item-output");
+
+function craftAll(craftingArray) {
+    output.innerHTML = "";
+    let craftCombinations = [];
+    for (let i = 0; i < craftingArray.length; i++) {
+        const element = craftingArray[i];
+        let item1 = element.currentItem;
+        for (let j = 0; j < craftingArray.length - i; j++) {
+            const e = craftingArray[j+i];
+            if (e.currentItem != element.currentItem) {
+                let item2 = e.currentItem;
+                craftCombinations.push([item1,item2]);
+            }
+        }
+    }
+    
+    // return craftCombinations;
+    let craftedItems = [];
+    for (let i = 0; i < craftCombinations.length; i++) {
+        let combinedValue = (craftCombinations[i][0]+1)*(craftCombinations[i][1]+1);
+        if (!contains(craftedItems,combinedValue)) {
+            craftedItems.push(combinedValue);
+            
+            let craftedItem = craftItem(craftCombinations[i][0],craftCombinations[i][1]);
+            //console.log(craftedItem);
+            if (craftedItem !== -1) {
+                let recepie = document.createElement("div");
+                let items = document.createElement("div");
+                items.classList.add("top-items");
+                recepie.classList.add("recepie");
+                items.appendChild(createDecorativeItemBox(craftCombinations[i][0]));
+                items.appendChild(textElement(" + "));
+                items.appendChild(createDecorativeItemBox(craftCombinations[i][1]));
+                recepie.appendChild(items);
+                recepie.appendChild(textElement(" = "));
+                recepie.appendChild(createDecorativeItemBox(craftedItem, "result"));
+                output.appendChild(recepie);
+            }
+            
+        }
+    }
+}
+
+function contains(array, obj) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function createDecorativeItemBox(defaultItem = -1,class_ = undefined) {
+    let box = document.createElement("img");
+    box.classList.add("item-image","item-box-decorative");
+    if (class_ != undefined) {
+        box.classList.add(class_);
+    }
+    if (defaultItem != -1) {   
+        if (defaultItem.f == undefined) {   
+            box.title = items[defaultItem].name;
+            box.src = `./portraits/${items[defaultItem].name_id}_icon.png`;
+        } else {
+            box.title = `${defaultItem.f} Food Pips`
+            box.src = `./portraits/Food_pip_icon.png`;
+        }
+    } else {
+        box.src = `./portraits/Remove_icon.png`;
+    }
+    return box;
+}
+
+craftItemButton.addEventListener('click', (e) => {
+    if (craftingMode == 0) {    
+       craftAll(craftingArray);
+    }
+});
